3f6eb414a9a3184ce4078b16abe007066c7bc0c8
AsokTamang <ashoktmg2058@gmail.com>
2025-12-19 00:44:52 -0500

    req and res type

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import type { Express } from "express";
+import type { Express,Request,Response } from "express";
 import { pets } from "./data/pets.js";
 
 const PORT = 8000;
 const app: Express = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {  //declaring the type for both req and res
   res.json(pets);
 });
 
