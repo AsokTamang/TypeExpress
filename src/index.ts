0054f9b1a52bd7d232f833843387feed0779ff1c
AsokTamang <ashoktmg2058@gmail.com>
2025-12-19 00:21:39 -0500

    assigning the type

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,14 @@
-import express from 'express'
-import type {Express} from 'express'
+import express from "express";
+import type { Express } from "express";
+import { pets } from "./data/pets.js";
 
-const PORT = 8000
-const app:Express = express()
+const PORT = 8000;
+const app: Express = express();
 
-type Pet = {
-  name: string,
-  species: string,
-  adopted: boolean,
-  age: number
-} 
+app.get("/", (req, res) => {
+  res.json(pets);
+});
 
-const pets:Pet[] = [{
-  name: "Rubik",
-  species: "Cat",
-  adopted: true,
-  age: 3
-},{
-   name: "Juby",
-  species: "Dog",
-  adopted: true,
-  age: 2
-}]
-
-app.get('/', (req, res)=> {
-  res.json(pets)
-})
-
-app.listen(PORT, ():void =>{
-  console.log("Listening on port: ", PORT)
-})
+app.listen(PORT, (): void => {
+  console.log("Listening on port: ", PORT);
+});
