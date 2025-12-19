5c7cd236fd1b22297373c96a77451b897a536498
AsokTamang <ashoktmg2058@gmail.com>
2025-12-19 11:11:32 -0500

    404-catch all error

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
-import type { Express,Request,Response } from "express";
+import type { Express, Request, Response } from "express";
 import { pets } from "./data/pets.js";
-
+import cors from "cors";
+//cross-origin resource sharing, this means allowing resources or datas to be shared in different domain, protocal(http or https) and PORT
 const PORT = 8000;
 const app: Express = express();
-
-app.get("/", (req:Request, res:Response) => {  //declaring the type for both req and res
+app.use(cors());
+app.get("/", (req: Request, res: Response): void => {
+  //declaring the type for both req and res
   res.json(pets);
 });
+
+app.use((req: Request, res: Response):void => {res.status(404).json("Not Found")}); //using a middleware
 
 app.listen(PORT, (): void => {
   console.log("Listening on port: ", PORT);
