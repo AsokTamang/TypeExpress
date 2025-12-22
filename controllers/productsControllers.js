5f12c2c4d8386a6215b4cc2918e0841d603e322e
AsokTamang <ashoktmg2058@gmail.com>
2025-12-21 21:53:16 -0500

    getting all the products from DB

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -3,13 +3,22 @@ import { open } from "sqlite";
 import sqlite3 from "sqlite3";
 import { getDatabase } from "../utils/dataconnection";
 
-export async function getProducts(req, res) {}
+const db = await getDatabase();
+
+export async function getProducts(req, res) {
+  try {
+    let query = `SELECT * FROM products`;
+    const total = await db.all(query);
+    res.status(200).json(total);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
 export async function getGenres(req, res) {
-  const db =await getDatabase();
   try {
     const genres = await db.all("SELECT DISTINCT genre FROM products"); //here we are using distinct inorder to avoid duplicate genres
-    const final = genres.map((genre) => Object.values(genre).toString()); //here we are converting the values of each object into string and storing as an array here in var final
+    const final = genres.map((row) => row.genre); //here we are converting the values of each object into string and storing as an array here in var final
     res.status(200).json(final);
     console.log(final);
   } catch (error) {
