70ded12a4e13f8b422d383139f481059c720ba15
AsokTamang <ashoktmg2058@gmail.com>
2025-12-22 13:30:56 -0500

    added the user's id in session

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 import validator from "validator";
 import { getDBConnection } from "../db/db.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 export async function registerUser(req, res) {
   let { name, username, email, password } = req.body;
   const regex = /^[a-zA-Z0-9_-]{1,20}$/;
-  const db=await getDBConnection();
+  const db = await getDBConnection();
 
   if (!name || !username || !email || !password) {
     res.status(400).json({ error: "All fields are required" });
@@ -19,28 +19,34 @@ export async function registerUser(req, res) {
     console.log("Invalid email");
     return;
   }
-  if (!regex.test(username)) {    //here we are using the .test method on regex so that the username must consists of these characters inside the regex only
+  if (!regex.test(username)) {
+    //here we are using the .test method on regex so that the username must consists of these characters inside the regex only
     res.status(400).json({ error: "Invalid username" });
     console.log("Invalid username");
     return;
   }
   try {
-    const condition = await db.get(`SELECT * FROM users WHERE username = ? OR email = ?`,[username,email])
-  if (condition!==undefined) {
-    return res.status(409).json({ error: "Email or username already in use." });
-  }
-  else{
-    //as we are inserting single data into database, we are using db.run() here
-    const hasedPW = await bcrypt.hash(password,10)  //here we are using brypt inorder to hash the password passed from UI
-    await db.run(`INSERT INTO users (name,email,username,password)  
-        VALUES(?,?,?,?)`,[name,email,username,hasedPW]);
-        return res.status(201).json({message:'User registered'})
-        
-  }
-    
+    const condition = await db.get(
+      `SELECT * FROM users WHERE username = ? OR email = ?`,
+      [username, email]
+    );
+    if (condition !== undefined) {
+      return res
+        .status(409)
+        .json({ error: "Email or username already in use." });
+    } else {
+      //as we are inserting single data into database, we are using db.run() here
+      const hasedPW = await bcrypt.hash(password, 10); //here we are using brypt inorder to hash the password using costfactor of 10 passed from UI
+      const result = await db.run(
+        `INSERT INTO users (name,email,username,password)  
+        VALUES(?,?,?,?)`,
+        [name, email, username, hasedPW]
+      );
+      req.session.userId = result.lastID; //this lastID is provided by sqlite3 or any sql which is the id of the users i.e primary key in nature
+
+      return res.status(201).json({ message: "User registered" });
+    }
   } catch (error) {
-    return res.status(500).json({error:'Network error'})
-    
+    return res.status(500).json({ error: "Network error" });
   }
-  
 }
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import { productsRouter } from "./routes/products.js";
 import { authRouter } from "./routes/auth.js";
-
+import session from "express-session";
 const app = express();
+const secret = process.env.secret;
 app.use(express.static("public")); //here we are using the built-in middleware of express in our app
 app.use("/api/products", productsRouter);
-app.use(express.json())  //here express.json() middleware is used for converting any req body into json
-app.use('/api/auth',authRouter)  // /api/auth gonna be the default route for our authentication
+app.use(express.json()); //here express.json() middleware is used for converting any req body into json
+app.use(
+  session({
+    secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,   //here we are using httponly to true so that cookie cannot be accessed via javascript which prevents from hacking of data
+      secure: false,      //as we are in dev mode , we are setting secure or https to false
+      sameSite: 'lax',
+    },
+  })
+);
+app.use("/api/auth", authRouter); // /api/auth gonna be the default route for our authentication
 const PORT = 8000;
 app
   .listen(PORT, () =>
